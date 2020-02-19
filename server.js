54525f5c34007139d7fd8abd32034ff53365873d
Cauany Souza <souza.cauany99@gmail.com>
2020-02-19 15:03:22 -0300

    configurando o servidor para pegar as informações do formulario pelo method post

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ nunjucks.configure("./", {
 //configurar o servidor para apresentar arquivos estáticos
 server.use(express.static('public'))
 
+//habilitar body do formulário
+server.use(express.urlencoded({ extended: true }))
+
 //configurando a lista de doadores: Vetor ou Array
 const donors = [
     {
@@ -32,14 +35,23 @@ const donors = [
     }
 ]
 
-
-
 //Configurar a apresentação da página
 server.get("/", function(req, res) {
     return res.render("index.html", { donors })
 })
 
-
+server.post("/", function(req, res){
+    //pegar dados do formulário
+    const name = req.body.name
+    const email = req.body.email
+    const blood = req.body.blood
+    //colocando valores dentro do array
+    donors.push({ 
+        name: name,
+        blood: blood,
+    })
+    return res.redirect("/")
+}
 //Ligando o servidor e permitir o acesso na porta 3000
 server.listen(3000, function() {
     console.log("Iniciei o servidor")
