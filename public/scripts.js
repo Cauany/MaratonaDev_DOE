d5b8a74c39366362ce65371bd2a4e5bc98586444
Cauany Souza <souza.cauany99@gmail.com>
2020-06-30 15:44:06 -0300

    validação feita em todos os campos

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,4 @@
  document.querySelector('header button').addEventListener("click", function(){
     document.querySelector('.form').classList.toggle('hide')
+    document.querySelector('section h2').classList.add('move')
  });
--- public/validation.js
+++ public/validation.js
@@ -1,32 +1,60 @@
-// const name = document.querySelector('form input[name="name"')
-// const email = document.querySelector('form input[name="email"')
-// const blood = document.querySelector('form select')
-// const button = document.querySelector('form button')
-const form = document.querySelector('form')
-const inputs = form.querySelectorAll('input').value
+const form = document.querySelector('section form')
 
-for(input in inputs){
-    console.log(input)
-    input.addEventListener('blur', onblurInput)
-    input.addEventListener('focus', onFocusInput)
-}
+form.addEventListener('submit', validation)
 
-function onblurInput(){
+function validation(e){
+    e.preventDefault()
     
-    if(input.value == ''){
-        input.style.backgroundColor = 'red'
-        input.value = 'Campo inválido'
-        email.style.marginTop = '5px'
-    }else{
-        input.style.backgroundColor = 'transparent'
-        form.querySelector('span').innerText = ''
-        email.style.marginTop = ''
-    }
-}
+    const name = form.querySelector('input[name="name"]')
+    const email = form.querySelector('input[name="email"]')
+    const blood = form.querySelector('select')
 
-function onFocusInput(){
+    const posOption = blood.options.selectedIndex
+
+    function validaCampo(){
+        if(name.value == ''){
+            form.querySelector('span#one')
+                .innerHTML = '* Campo inválido'
+            name.style.border = '2px solid red'
+            email.classList.add('space')
+        }else{
+            form.querySelector('span#one')
+                .innerHTML = ''
+            name.style.border = '1px solid #ffffff88'
+            email.classList.remove('space')
+        }
     
-    if(!(input.value == '')){
-        input.style.backgroundColor = ''
+        if(email.value == ''){
+            form.querySelector('span#two')
+                .innerHTML = '* Campo inválido'
+            email.style.border = '2px solid red'
+            blood.classList.add('space')
+        }else{
+            form.querySelector('span#two')
+                .innerHTML = ''
+            email.style.border = '1px solid #ffffff88'
+            blood.classList.remove('space')
+        }
+    
+        if(posOption == 0){
+            form.querySelector('span#three')
+                .innerHTML = '* Campo inválido'
+            blood.style.border = '2px solid red'
+            form.querySelector('button')
+                .classList.add('space')
+        }else{
+            form.querySelector('span#three')
+                .innerHTML = ''
+            blood.style.border = '1px solid #ffffff88'
+            form.querySelector('button')
+                .classList.remove('space')
+        }
     }
+
+    validaCampo()
+
+    if(!(name.value == '' && email.value == '')){
+        if(posOption == 0) return alert(' Campo tipo sanguíneo inválido')
+        form.submit()
+    } 
 }
