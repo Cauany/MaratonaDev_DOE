73d4b1edf31cb278ec1e8cc8656f17f843828ab1
Cauany <souza.cauany99@gmail.com>
2021-09-17 19:03:21 -0300

    Removed yarn.lock, updated package.json and chnged db config

--- db.js
+++ db.js
@@ -1,11 +1,19 @@
 //configurar a conexão com o banco de dados
-const Pool = require('pg').Pool
-const db = new Pool({ 
-    user: 'postgres',
-    password: 'cicerasouza0708',
-    host: 'localhost',
-    port: 5432,
-    database: 'Doe'
+const { Pool } = require('pg')
+
+// const db = new Pool({ 
+//     user: 'postgres',
+//     password: 'cicerasouza0708',
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'Doe'
+// })
+
+const url = 'postgres://znbwhhivtddpkm:dc7151cbb63bf9eee229bb6c0882a8ff552bd0ce3aadfc4d6d16d1232498c8fc@ec2-52-2-118-38.compute-1.amazonaws.com:5432/ddo5hvi6b1l235'
+
+module.exports = new Pool({
+  connectionString: url,
+  ssl: { rejectUnauthorized: false },
 })
 
 module.exports = db
